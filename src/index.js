9aea53322083dd00dec97a5256b7329e74dd2270
HuangZhiming-the-Great <740616790@qq.com>
2020-12-03 17:51:00 +0800

    second piblish.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import _ from 'lodash';
+
 function component(){
   var element = document.createElement('div');
   
   //Lodash (_.join()引入)对于执行这一行是必须的。
+  //现在lodash以import的形式引入。
   element.innerHTML = _.join(['Hello','webpack'],' ');
   return element;
 }
