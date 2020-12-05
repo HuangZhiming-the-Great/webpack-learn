a634cf23e8dab6ed7dc4cde1f043500232ed19a3
HuangZhiming-the-Great <740616790@qq.com>
2020-12-05 17:00:15 +0800

    learnto load the files of pictures fonts data and so on.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import _ from 'lodash';
-
+import './style.css';
+import icon from './icon.jpeg';
+import data from './data.xml';
 function component(){
   var element = document.createElement('div');
   
   //Lodash (_.join()引入)对于执行这一行是必须的。
   //现在lodash以import的形式引入。
   element.innerHTML = _.join(['Hello','webpack'],' ');
+  element.classList.add('hello');
+
+  //将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src=icon;
+
+  element.appendChild(myIcon);
+
+  console.log(data);
+  
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,43 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname,'dist')
+  },
+  module: {
+    rules: [
+      //webpack根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的loader。在这种情况下，以css结尾的全部文件，都将被提供给 style-loader和css-loader。
+      {
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      //事实上 file-loader和url-loader可以接收并加载任何文件，然后将其输出带构建目录。
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      //如果是加载csv和xml的话，我们使用csv-loader和xml-loader。
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
   }
 };
